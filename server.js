60eec2b617cd6e128a9708c3d60dd1f5a90aba02
Evan Arbour <evan.arbour@gmail.com>
2021-08-26 12:58:35 -0700

    created routes file and added uuid to package.json

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+// const api = require('./public/assets/js/index.js');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+// app.use('/api', api);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for landing page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
