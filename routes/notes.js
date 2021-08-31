8135873b8b58a7de20444af5461ef861d47c46e6
Evan Arbour <evan.arbour@gmail.com>
2021-08-30 17:58:00 -0700

    connected the db.json database to the front end

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,48 @@
-const notes = require('express').Router();
-// helper file with code to write, append etc
+const fs = require('fs');
 
 // uuid module to create unique ID for each note
 const { v4: uuidv4 } = require('uuid');
 
-// GET Route to retrieve notes from database
 
-// POST Route for new note >> appends to database 
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+        console.log('\n Working on GET request..');
+    
+        // read the db.json file
+        let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    
+        console.log('\n Notes data here: \n' + JSON.stringify(data));
+    
+        // return the data 
+        res.json(data);
+    });
+    
+    // POST route for note data
+    app.post('/api/notes', (req, res) => {
+    
+        // set request body for data to be added
+        const newNote = req.body;
+    
+        console.log('\n POST request for New Note: ' + JSON.stringify(newNote));
+    
+        // assign unique id to the note being created 
+        newNote.id = uuidv4();
+        
+        // read the db.json file
+        let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    
+        // push the 'currentNote' into existing db.json file
+        data.push(newNote);
+    
+        // update the db.json database with new data including 'currentNote'
+        fs.writeFileSync('./db/db.json', JSON.stringify(data))
+    
+        console.log('\n Note added to database!');
+    
+        res.json(data);
+    
+    });
+}
 
 
-module.exports = notes;
+
--- server.js
+++ server.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const path = require('path');
 
-const api = require('./routes/router');
-
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', api);
 
-// GET Route for notes page
+require('./routes/notes')(app);
+
+// GET Route for notes HTML page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET Route for landing page
+// GET Route for index HTML page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
